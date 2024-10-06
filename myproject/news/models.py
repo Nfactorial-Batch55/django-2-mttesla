6eb99b4824c9af46ed59ee0d18329a60920483b1
mttesla <fincher0307@gmail.com>
2024-10-06 23:23:53 +0500

    add new folders, edit models.py, urls.py, forms.py

--- myproject/news/models.py
+++ myproject/news/models.py
@@ -8,3 +8,12 @@ class News(models.Model):
 
     def __str__(self):
         self.title = title
+
+
+class Comment(models.Model):
+    news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.news.tetle}'
--- myproject/news/views.py
+++ myproject/news/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import News
-from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import News, Comment
 from django.utils import timezone
-from .forms import NewsForm
+from .forms import NewsForm, CommentForm
 
 # Create your views here.
 def news_list(request):
@@ -12,7 +11,25 @@ def news_list(request):
 
 def news_detail(request, pk):
     news = get_object_or_404(News, pk=pk)
-    return render(request, 'news/news_detail.html', {'news': news})
+    comments = news.comments.all().order_by('-created_at')
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.news = news
+            comment.created_at = timezone.now()
+            comment.save()
+            return redirect('news_detail', pk=news.pk)
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'news/news_detail.html', {
+        'news': news,
+        'comments': comments,
+        'comment_form': comment_form
+    })
+
 
 def add_news(request):
     if request.method == 'POST':
